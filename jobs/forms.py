9c3df212d19a9ec4988708cf553ce8c8c57285cc
Stephen Withrow <swithrow@webucator.com>
2021-01-02 12:47:52 -0500

    commit

--- jobs/forms.py
+++ jobs/forms.py
@@ -1,14 +1,4 @@
-from datetime import datetime
 from django import forms
-from django.core.exceptions import ValidationError
-from django.core.validators import URLValidator
-
-def validate_future_date(value):
-    if value < datetime.now().date():
-        raise ValidationError(
-            message=f'{value} is in the past.', code='past_date'
-        )
-    
 
 class JobApplicationForm(forms.Form):
     EMPLOYMENT_TYPES = (
@@ -26,46 +16,20 @@ class JobApplicationForm(forms.Form):
         (5, 'FRI')
     )
     
-    YEARS = range(datetime.now().year, datetime.now().year+2)
-
-    first_name = forms.CharField(
-       widget=forms.TextInput(attrs={'autofocus': True})
-    )
+    first_name = forms.CharField()
     last_name = forms.CharField()
     email = forms.EmailField()
-    website = forms.CharField(
-        required=False,
-        widget=forms.TextInput(
-            attrs={'placeholder':'https://www.example.com', 'size':'50'}
-        ),
-        validators=[URLValidator(schemes=['http', 'https'])]
-    )
+    website = forms.URLField(required=False)    
     employment_type = forms.ChoiceField(choices=EMPLOYMENT_TYPES)
     start_date = forms.DateField(
-        help_text='The earliest date you can start working.',
-        widget=forms.SelectDateWidget(
-            years=YEARS,
-            attrs={'style': 'width: 31%; display: inline-block; margin: 0 1%'}
-        ),
-        validators = [validate_future_date],
-        error_messages = {'past_date': 'Please enter a future date.'}
+        help_text='The earliest date you can start working.'
     )
-    available_days = forms.TypedMultipleChoiceField(
+    available_days = forms.MultipleChoiceField(
         choices=DAYS,
-        coerce=int,
-        help_text='Check all days that you can work.',
-        widget=forms.CheckboxSelectMultiple(
-            attrs={'checked':True}
-        )
+        help_text='Check all days that you can work.'
     )
-    desired_hourly_wage = forms.DecimalField(
-        widget=forms.NumberInput(
-            attrs={'min':'10.00', 'max':'100.00', 'step':'.25'}
-        )
-    )
-    cover_letter = forms.CharField(
-        widget=forms.Textarea(attrs={'cols': '75', 'rows': '5'})
-    )
+    desired_hourly_wage = forms.DecimalField()
+    cover_letter = forms.CharField()
     confirmation = forms.BooleanField(
         label='I certify that the information I have provided is true.'
     )
