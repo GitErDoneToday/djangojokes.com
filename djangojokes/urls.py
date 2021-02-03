61118131ad8b06b4c95756b88e3f79d7ae1d46c1
Stephen Withrow <swithrow@webucator.com>
2021-02-02 20:02:08 -0500

    e59

--- djangojokes/urls.py
+++ djangojokes/urls.py
@@ -2,7 +2,6 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-import private_storage.urls
 
 urlpatterns = [
     # Admin
@@ -12,9 +11,6 @@ urlpatterns = [
     # User Management
     path('account/', include('users.urls')),
     path('account/', include('allauth.urls')),
-
-    # private media
-    path('media/private/', include(private_storage.urls)),
 
     # Local Apps
     path('jobs/', include('jobs.urls')),
