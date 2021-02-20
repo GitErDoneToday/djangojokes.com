59f37fe24abb7c18fa8c430de7fff15d1547ef72
Stephen Withrow <swithrow@webucator.com>
2021-02-20 17:38:17 -0500

    e77

--- jokes/admin.py
+++ jokes/admin.py
@@ -25,6 +25,10 @@ class JokeAdmin(DjangoJokesAdmin):
     ordering = ['-updated']
     search_fields = ['question', 'answer']
 
+    # Form attributes
+    autocomplete_fields = ['tags', 'user']
+    radio_fields = { 'category': admin.HORIZONTAL}
+
     def get_readonly_fields(self, request, obj=None):
         if obj: # editing an existing object
             return ('slug', 'created', 'updated')
@@ -46,6 +50,7 @@ class JokeVoteAdmin(admin.ModelAdmin):
 class TagAdmin(admin.ModelAdmin):
     model = Tag
     list_display = ['tag', 'created', 'updated']
+    search_fields = ['tag']
 
     def get_readonly_fields(self, request, obj=None):
         if obj: # editing an existing object
