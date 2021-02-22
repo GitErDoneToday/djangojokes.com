9191d3a788e50d58c053a3cdba2fbfcb5eded600
Stephen Withrow <swithrow@webucator.com>
2021-02-22 13:39:43 -0500

    E80

--- users/admin.py
+++ users/admin.py
@@ -1,46 +1,48 @@
 from django.contrib import admin
 from django.contrib.auth import get_user_model
 from django.contrib.auth.admin import UserAdmin
+from django.utils.safestring import mark_safe
+from django.urls import reverse
+
 from common.admin import DjangoJokesAdmin
 from common.utils.admin import append_fields, move_fields, remove_fields
 
-User = get_user_model()
+CustomUser = get_user_model()
 
-@admin.register(User)
+@admin.register(CustomUser)
 class CustomUserAdmin(DjangoJokesAdmin, UserAdmin):
-    model = User
+    model = CustomUser
 
-    # List attributes
+    # List Attributes
     list_display = UserAdmin.list_display + ('is_superuser',)
     list_display_links = ('username', 'email', 'first_name', 'last_name')
-    ''' remove in Exercise:
-    add_fieldsets = UserAdmin.add_fieldsets + (
-        ('Optional Fields', {
-            'classes': ('wide',),
-            'fields': ('email', 'first_name', 'last_name'),
-        }),
-    ) '''
+
+    readonly_fields = ['password_form']
 
     # Fields for editing existing user.
     new_fields = ('dob', 'avatar')
-    # Add new fields to 'Personal info' fieldset.
+    # Add new fields to 'Personal info' section.
     append_fields(UserAdmin.fieldsets, 'Personal info', new_fields)
-    # Move email field from 'Personal Info' fieldset to unlabelled fieldset.
-    move_fields(UserAdmin.fieldsets, 'Personal info', None, ('email',))  
-    # Remove password field.
-    remove_fields(UserAdmin.fieldsets, None, ('password',)) 
+    # Move email field from 'Personal info' section to unlabelled section
+    move_fields(UserAdmin.fieldsets, 'Personal info', None, ('email',))
+    # Remove password field from 'Personal info'
+    remove_fields(UserAdmin.fieldsets, None, ('password',))
+    append_fields(UserAdmin.fieldsets, None, ('password_form',))
 
     # Fields for adding new user.
-    new_fields = ('email',)
-    # Add new fields to unlabelled fieldset.
-    add_fieldsets = append_fields(UserAdmin.add_fieldsets, None, new_fields)
+    new_fields = ('email', )
+    # Add new fields to unlabelled section.
+    append_fields(UserAdmin.add_fieldsets, None, new_fields)
 
     # Add optional fields to new 'Optional Fields' section.
     optional_fields = ('first_name', 'last_name', 'dob')
-    add_fieldsets = append_fields(UserAdmin.add_fieldsets, 'Optional Fields', optional_fields)
+    append_fields(UserAdmin.add_fieldsets, 'Optional Fields', optional_fields)
 
-    # Add Save buttons to the top of the change user form.
+    def password_form(self, obj):
+        url = reverse('admin:auth_user_password_change', args=[obj.pk])
+        return mark_safe(f'<a href="{url}">Change Password</a>')
+
+    # Add Save buttons to the top of the change user form
     def get_form(self, request, obj=None, **kwargs):
         self.save_on_top = obj is not None
         return super().get_form(request, obj, **kwargs)
-
