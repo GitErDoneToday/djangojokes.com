5c5d8c7c780a4fe24fef51af05962a6712f72583
Stephen Withrow <swithrow@webucator.com>
2022-11-07 13:34:49 -0800

    Lesson updates

--- jokes/views.py
+++ jokes/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.views.generic import CreateView, DetailView, ListView, UpdateView
+from .models import Joke
 
-# Create your views here.
+class JokeCreateView(CreateView):
+    model = Joke
+    fields = ['question', 'answer']
+
+class JokeDetailView(DetailView):
+    model = Joke
+
+class JokeListView(ListView):
+    model = Joke
+
+class JokeUpdateView(UpdateView):
+    model = Joke
+    fields = ['question', 'answer']
